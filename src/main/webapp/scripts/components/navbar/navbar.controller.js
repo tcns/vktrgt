570d24e365cb50ab248855728ccfd952741c5a6a
NuclearShop <collaandspar@gmail.com>
2016-05-07 23:06:08 +0300

    navigation style

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -5,7 +5,20 @@ angular.module('vktrgtApp')
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
+        $RIGHT_COL = angular.element('.right_col')
+        $scope.setContentHeight = function () {
+            // reset height
+            $RIGHT_COL.css('min-height', $(window).height());
 
+            var bodyHeight = $BODY.height(),
+                leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+                contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
+
+            // normalize content
+            contentHeight -= $NAV_MENU.height() + $FOOTER.height();
+
+            $RIGHT_COL.css('min-height', contentHeight);
+        };
         $scope.logout = function () {
             Auth.logout();
             $state.go('home');
--- src/main/webapp/scripts/components/navbar/navbar.directive.js
+++ src/main/webapp/scripts/components/navbar/navbar.directive.js
@@ -20,6 +20,66 @@ angular.module('vktrgtApp')
             }
         };
     })
+    .directive('dropdownLink', function() {
+        return {
+            restrict: 'A',
+            transclude: false,
+            link: function (scope, element, attrs) {
+                $SIDEBAR_MENU = angular.element('#sidebar-menu');
+                element.on('click', function(ev) {
+                    var $li = element.parent();
+
+                    if ($li.is('.active')) {
+                        $li.removeClass('active');
+                        angular.element('ul:first', $li).slideUp(function() {
+                            scope.setContentHeight();
+                        });
+                    } else {
+                        // prevent closing menu if we are on child menu
+                        if (!$li.parent().is('.child_menu')) {
+                            $SIDEBAR_MENU.find('li').removeClass('active');
+                            $SIDEBAR_MENU.find('li ul').slideUp();
+                        }
+
+                        $li.addClass('active');
+
+                        angular.element('ul:first', $li).slideDown(function() {
+                            scope.setContentHeight();
+                        });
+                    }
+                });
+            }
+        };
+    })
+    .directive('menuToggle', function() {
+        return {
+            restrict: 'A',
+            transclude: false,
+            link: function (scope, element, attrs) {
+                $SIDEBAR_MENU = angular.element('#sidebar-menu');
+                $LEFT_COL = angular.element('.left_col');
+                $BODY = angular.element('body')
+                element.on('click', function() {
+                    if ($BODY.hasClass('nav-md')) {
+                        $BODY.removeClass('nav-md').addClass('nav-sm');
+                        $LEFT_COL.removeClass('scroll-view').removeAttr('style');
+
+                        if ($SIDEBAR_MENU.find('li').hasClass('active')) {
+                            $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+                        }
+                    } else {
+                        $BODY.removeClass('nav-sm').addClass('nav-md');
+
+                        if ($SIDEBAR_MENU.find('li').hasClass('active-sm')) {
+                            $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+                        }
+                    }
+
+                    scope.setContentHeight();
+                })
+            }
+        };
+    })
     .directive('activeLink', function(location) {
         return {
             restrict: 'A',
