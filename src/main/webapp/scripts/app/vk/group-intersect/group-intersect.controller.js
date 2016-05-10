4ffeb6cede54cc61ce79e854d5252e18654eb53b
NuclearShop <collaandspar@gmail.com>
2016-05-11 00:31:43 +0300

    group activity validation

--- src/main/webapp/scripts/app/vk/group-intersect/group-intersect.controller.js
+++ src/main/webapp/scripts/app/vk/group-intersect/group-intersect.controller.js
@@ -5,11 +5,11 @@ angular.module('vktrgtApp')
     .controller('GroupIntersectController', function ($scope, VKCloudService) {
 
         $scope.submit = function () {
-                VKCloudService.intersectGroups($scope.groups.split('\n'),
-                    $scope.taskInfo).success(function (response) {
-                        $scope.message = "Задача добавлена";
-                    }).error(function (response) {
-                        $scope.message = "Произошла ошибка";
-                    })
+            VKCloudService.intersectGroups($scope.groups.split('\n'),
+                $scope.taskInfo).success(function (response) {
+                    $scope.message = "Задача добавлена";
+                }).error(function (response) {
+                    $scope.message = "Произошла ошибка";
+                })
         };
     });
