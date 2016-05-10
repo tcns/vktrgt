1ec8123ad28166c10561c7653af5cd23c36bf445
tcns <Nbvehxbr1994>
2016-05-10 19:35:55 +0300

    add validation for group intersection

--- src/main/webapp/scripts/app/vk/group-intersect/group-intersect.controller.js
+++ src/main/webapp/scripts/app/vk/group-intersect/group-intersect.controller.js
@@ -5,11 +5,14 @@ angular.module('vktrgtApp')
     .controller('GroupIntersectController', function ($scope, VKCloudService) {
 
         $scope.submit = function () {
-            VKCloudService.intersectGroups($scope.groups.split('\n'),
-                $scope.taskInfo).success(function(response){
-                    $scope.message = "Задача добавлена";
-                }).error(function(response){
-                    $scope.message = "Произошла ошибка";
-                })
+            if ($scope.groupIntersectForm.$valid) {
+                VKCloudService.intersectGroups($scope.groups.split('\n'),
+                    $scope.taskInfo).success(function (response) {
+                        $scope.message = "Задача добавлена";
+                    }).error(function (response) {
+                        $scope.message = "Произошла ошибка";
+                    })
+            }
+
         };
     });
